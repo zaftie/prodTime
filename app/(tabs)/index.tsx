1b94b3f938cdfdb7c256341bd2cdf93d5417a2ab
zaftie <51311245+zaftie@users.noreply.github.com>
2024-12-28 14:46:04 -0800

    bio

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -42,13 +42,7 @@ export default function HomeScreen() {
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+
       </ThemedView>
     </ParallaxScrollView>
   );
